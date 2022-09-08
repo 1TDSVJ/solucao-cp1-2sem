0489f0f08a8b080e3a38d61e09e9aecae05378d3
viniciusreis <rm94707@fiap.com.br>
2022-09-08 09:56:08 -0300

    Iniciando o SASS

--- rm94707/cp1/src/app.jsx
+++ rm94707/cp1/src/app.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
-import MediaAluno from "./components/mediaAluno/MediaAluno";
+import React from "react"
+import { useState } from "react"
+import MediaAluno from "./componentes/mediaAluno/MediaAluno"
 
 export default function App() {
-  const [aluno, setAluno] = useState({
-    nome: "Joaquim",
-    rm: 99999,
-    turma: "1TDSZ",
-  });
 
-  const nota1 = 10;
-  const nota2 = 10;
-  const nota3 = 10;
+    const [aluno, setAluno] = useState({
+        nome: "Vinicius Reis",
+        rm: 94707,
+        turma: "1TDSVJ"
+    })
 
-  return (
-    <>
-      <h1>CP1 de RWD 1º SEMESTRE</h1>
-      <MediaAluno aluno={aluno} nota1={nota1} nota2={nota2} nota3={nota3} />
-    </>
-  );
+    const nota1 = 7
+    const nota2 = 6
+    const nota3 = 8
+
+    return (
+        <>
+            <h1>CP1 de RWD 1º SEMESTRE</h1>
+
+            <div className="container">
+                <p>Apresentando com SASS</p>
+            </div>
+            
+            <MediaAluno alunoProps={aluno} nota1Props={nota1} nota2Props={nota2} nota3Props={nota3} />
+        </>
+    )
 }
+
+
+// 2 – No App.jsx crie a seguinte estrutura. ( 3 Pontos)
+// a) Crie um fragmento.
+// b) Um elemento h1 com o texto “CP1 de RWD 1º SEMESTRE”.
+// c) Declare um useState de nome aluno que recebe um objeto do tipo aluno com os
+// seguintes atributos:
+// 1 – nome = Joaquim
+// 2 – rm = 99999
+// 3 – turma = 1TDSZ
+// d) Três constantes declaradas como nota1, nota2 e nota3.
--- rm94707/cp1/src/index.js
+++ rm94707/cp1/src/index.js
@@ -1,5 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './css/main.scss'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+)
